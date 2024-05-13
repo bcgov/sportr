ff22613f2541c57c03dcaebce8be2e5f23aff205
Kiri <kiridaust@gmail.com>
2024-05-12 21:59:24 -0700

    minor

--- portfolio_fns.R
+++ portfolio_fns.R
@@ -171,6 +171,7 @@ get_portfolio <- function(portfolio_raw, run_list,
 }
 
 plot_ef <- function(portfolio, run_list, current_unit, returnValue = 0.9){
+  portfolio <- portfolio[!is.infinite(value),]
   efAll <- dcast(portfolio,Return ~ Spp, fun.aggregate = function(x){sum(x)/(length(run_list))})
   efAll <- na.omit(efAll)
   RetCurve <- stats::approx(efAll$RealRet,efAll$Sd,xout = returnValue)
